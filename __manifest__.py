83307bc7cf5b862e057f9120bae7fa8b1af360d7
masmtz <s.mtz.casillas@gmail.com>
2021-02-09 13:33:28 -0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Formatos Toljy',
+    'summary': 'Este módulo agrega los formatos de Pedido de venta, Factura y compras Toljy.',
+    'author': 'Samuel Martinez, Morwi Encoders Consulting SA DE CV',
+    'website': 'http://www.morwi.mx/',
+    'category': 'Hidden',
+    'version': '14.0.1.0.0',
+    'depends': ['base'],
+    'data': [
+        'views/invoice_document.xml',
+        'views/purchase_order.xml',
+        'views/report_sale_order.xml',
+        ],
+}
